a5111bcca0768284f755bab4270bb8909f487e19
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-03 18:19:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   
   def execute_transaction
     #binding.pry
-    if self.valid?
+    if valid?
       @sender.balance - @amount
       @receiver.balance + @amount
       @status = "complete"
